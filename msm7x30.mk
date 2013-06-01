ac0b55db3096267317259f385d4fa6c1fd014328
coderzstas <coderz35@gmail.com>
2013-06-01 18:50:16 +0300

    cleanup 2

--- msm7x30.mk
+++ msm7x30.mk
@@ -51,9 +51,6 @@ PRODUCT_COPY_FILES += \
     device/htc/msm7x30-common/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
     device/htc/msm7x30-common/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw
 
-PRODUCT_COPY_FILES += \
-    device/htc/msm7x30-common/configs/audio_policy.conf:system/etc/audio_policy.conf
-
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
